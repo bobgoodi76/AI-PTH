7e69a1522404d38fbdeb2a3a7e0f2564aab0ecf9
bobgoodi76 good <bobgoodsson@gmail.com>
2023-09-18 17:36:05 +0000

    first step

---
+++ Dag01/venvfirstai/exeer01.py
@@ -0,0 +1,18 @@
+data = [
+    {
+        `name`: `A`,
+        `version`:`1.0.5`
+    },
+    {
+        `name`: `B`,
+        `version`:`1.0.7`
+    },
+    {
+        `name`: `C`,
+        `version`:`1.0b.0`
+    },
+     {
+        `name`: `D`,
+        `version`:`1.0a.0`
+    },
+]
